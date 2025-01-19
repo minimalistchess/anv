5f06b830c5fb813f17dccca12734b6465dec0739
minimalistchess <131514555+minimalistchess@users.noreply.github.com>
2025-01-19 21:53:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 // Placeholder for potential interactivity or dynamic features in the future
-if (window.location.pathname === "/index.html") {
-  window.location.replace("/");
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Website loaded successfully.');
 });
  document.getElementById("year").textContent = new Date().getFullYear();
+ 
 document.addEventListener("DOMContentLoaded", () => {
   new Swiper(".swiper-container", {
     loop: true,
