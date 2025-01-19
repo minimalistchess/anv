3f9bdcbef3e21136b1804ef90db072479b8201f5
minimalistchess <131514555+minimalistchess@users.noreply.github.com>
2025-01-19 21:26:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('Website loaded successfully.');
 });
  document.getElementById("year").textContent = new Date().getFullYear();
+document.addEventListener("DOMContentLoaded", () => {
+  new Swiper(".swiper-container", {
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+});
